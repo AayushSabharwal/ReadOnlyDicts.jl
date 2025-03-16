79892032a4cead5fda78980d64c5247d19468804
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-03-16 13:17:19 +0530

    test: add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,16 @@ using ReadOnlyDicts
 using Test
 using Aqua
 using JET
+using SafeTestsets
 
 @testset "ReadOnlyDicts.jl" begin
     @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(ReadOnlyDict)
+        Aqua.test_all(ReadOnlyDicts)
     end
     @testset "Code linting (JET.jl)" begin
-        JET.test_package(ReadOnlyDict; target_defined_modules = true)
+        JET.test_package(ReadOnlyDicts; target_defined_modules = true)
     end
-    # Write your tests here.
+    @safetestset "ReadOnlyDict tests" begin
+        include("core.jl")
+    end
 end
